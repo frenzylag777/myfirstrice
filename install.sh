aa752df6f0671f1482a29b56e94dc25051e9a379
frenzylag777 <107318308+frenzylag777@users.noreply.github.com>
2026-04-12 21:46:02 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Warning! This will delete your current configs in ~/.config/"
+read -p "Type 'yes install' to proceed: " answer
+
+if [ "$answer" = "yes install" ]; then
+    echo "Installation started..."
+
+    rm -rf "$HOME/.config/alacritty" "$HOME/.config/hypr" "$HOME/.config/waybar" "$HOME/.config/fastfetch"
+
+    mkdir -p "$HOME/.config/alacritty" "$HOME/.config/hypr" "$HOME/.config/waybar" "$HOME/.config/fastfetch"
+
+    cp -r alacritty/* "$HOME/.config/alacritty/"
+    cp -r hypr/* "$HOME/.config/hypr/"
+    cp -r fastfetch/* "$HOME/.config/fastfetch/"
+    cp -r waybar/* "$HOME/.config/waybar/"
+    cp zshrc "$HOME/.zshrc"
+
+    echo "Success! Dotfiles installed."
+else
+    echo "Installation cancelled. You must type 'yes install' exactly."
+    exit 1
+fi
